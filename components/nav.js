698c5ff637f893b863f11b790a0be4a2bac939a9
Zach Seaman <znseaman@gmail.com>
2019-12-13 12:55:41 -0800

    Add website structure

---
+++ components/nav.js
@@ -0,0 +1,59 @@
+import React from "react";
+import Link from "next/link";
+
+const links = [
+  { href: "/projects", label: "Projects" },
+  { href: "/about", label: "About" }
+].map(link => {
+  link.key = `nav-link-${link.href}-${link.label}`;
+  return link;
+});
+
+const Nav = () => (
+  <nav>
+    <ul>
+      <li>
+        <Link href="/">
+          <a>Zachary Seaman</a>
+        </Link>
+      </li>
+      {links.map(({ key, href, label }) => (
+        <li key={key} className={label.toLowerCase()}>
+          <a href={href}>{label}</a>
+        </li>
+      ))}
+    </ul>
+
+    <style jsx>{`
+      :global(body) {
+        margin: 0;
+        font-family: -apple-system, BlinkMacSystemFont, Avenir Next, Avenir,
+          Helvetica, sans-serif;
+      }
+      nav {
+        text-align: center;
+      }
+      ul {
+        display: flex;
+        justify-content: flex-end;
+      }
+      nav > ul {
+        padding: 4px 16px;
+      }
+      li {
+        display: flex;
+        padding: 6px 8px;
+      }
+      a {
+        color: #067df7;
+        text-decoration: none;
+        font-size: 13px;
+      }
+      li:first-child {
+        margin-right: auto;
+      }
+    `}</style>
+  </nav>
+);
+
+export default Nav;
---
+++ pages/projects.js
@@ -0,0 +1,83 @@
+import React from "react";
+import Head from "next/head";
+import Nav from "../components/nav";
+
+const Projects = () => (
+  <div>
+    <Head>
+      <title>Projects</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <Nav />
+    <div className="hero">
+      <h1 className="title">Projects</h1>
+
+      <div className="row">
+        <a href="https://nextjs.org/docs" className="card">
+          <h3>Project 1 &rarr;</h3>
+          <p>Short description of the project</p>
+        </a>
+        <a href="https://nextjs.org/learn" className="card">
+          <h3>Project 2 &rarr;</h3>
+          <p>Short description of the project</p>
+        </a>
+        <a
+          href="https://github.com/zeit/next.js/tree/master/examples"
+          className="card"
+        >
+          <h3>Project 3 &rarr;</h3>
+          <p>Short description of the project</p>
+        </a>
+      </div>
+    </div>
+
+    <style jsx>{`
+      .hero {
+        width: 100%;
+        color: #333;
+      }
+      .title {
+        margin: 0;
+        width: 100%;
+        padding-top: 80px;
+        line-height: 1.15;
+        font-size: 48px;
+      }
+      .title,
+      .description {
+        text-align: center;
+      }
+      .row {
+        max-width: 880px;
+        margin: 80px auto 40px;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-around;
+      }
+      .card {
+        padding: 18px 18px 24px;
+        width: 220px;
+        text-align: left;
+        text-decoration: none;
+        color: #434343;
+        border: 1px solid #9b9b9b;
+      }
+      .card:hover {
+        border-color: #067df7;
+      }
+      .card h3 {
+        margin: 0;
+        color: #067df7;
+        font-size: 18px;
+      }
+      .card p {
+        margin: 0;
+        padding: 12px 0 0;
+        font-size: 13px;
+        color: #333;
+      }
+    `}</style>
+  </div>
+);
+
+export default Projects;
