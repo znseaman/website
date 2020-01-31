129d100b01b01fea2c95526f3256311a4cbf3fdd
Zach Seaman <znseaman@gmail.com>
2020-01-30 20:52:49 -0800

    fix: change text for project

--- pages/projects.js
+++ pages/projects.js
@@ -23,7 +23,7 @@ const Projects = () => (
         </a>
         <a href="/find_apartment" className="card">
           <h3>Find Apartment &rarr;</h3>
-          <p>A web application to find an apartment in Vancouver</p>
+          <p>A web application to find a place to rent in Vancouver</p>
         </a>
         {/* <a href="https://nextjs.org/learn" className="card">
           <h3>Project 2 &rarr;</h3>
